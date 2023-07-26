57221bf41346718359affcbd63301477c47a2618
akan <akanaydaraliev2002@gmail.com>
2023-07-26 14:53:13 +0600

    edit

--- core/settings.py
+++ core/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'mainapp',
+
     'rest_framework',
     'rest_framework.authtoken',
+    
     'django_filters',
 ]
 
